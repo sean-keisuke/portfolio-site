a73887adb722de0c1651047796d85d086b6052bc
Sean Mullarkey <45245505+sean-keisuke@users.noreply.github.com>
2021-11-10 13:58:54 -0700

    Delete Index.js

--- src/components/Index.js
+++
@@ -1,9 +0,0 @@
-export { default as Header } from "./Header";
-export { default as Footer } from "./Footer";
-export { default as BurgerMenu } from "./BurgerMenu";
-export { default as MenuModal } from "./MenuModal";
-export { default as Seo } from "./Seo";
-export { default as Theme } from "./Theme";
-export { default as Page } from "./Page";
-export { default as Projects } from "./Projects";
-export { default as IndividualProject} from "./IndividualProject"
